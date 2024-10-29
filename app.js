1b12fef9200232e6c70736064da7d5b7cc882f3c
Seiha H <tateygames.kh@gmail.com>
2024-10-29 16:45:23 +0700

    first commit

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
+require('dotenv').config();
+
 
 const app = express();
 
