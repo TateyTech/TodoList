9781ee2599cea868088083feb3f83ad6f7e6f2e1
Seiha H <tateygames.kh@gmail.com>
2024-10-29 17:10:13 +0700

    update app.js

--- index.js
+++ index.js
@@ -210,8 +210,9 @@ app.post("/delete", function (req, res) {
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, function () {
-  console.log(`Server started on port ${PORT}`);
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 // // Export the app for Vercel
